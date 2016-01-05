7928215fa02c6ab5aaab6de47390bbc556a02cef
regine22 <regine.aquino22@yahoo.com.ph>
2016-01-05 15:45:15 +0100

    test account

--- MDSD Pet Hotel/src/TestAccount.java
+++ MDSD Pet Hotel/src/TestAccount.java
@@ -1,4 +1,8 @@
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+import Classes.mdsdAccount.Account;
 import Classes.mdsdAccount.AccountController;
 import Classes.mdsdAccount.MdsdAccountFactory;
 import Classes.mdsdAdmin.AdminController;
@@ -16,8 +20,21 @@ public class TestAccount {
 	
 	@Test
 	public void testGetAccount() {
+		accountCtrl.createAccount("Regine", "regine@gmail.com", "lol");
+		Account account = accountCtrl.getAccount("regine@gmail.com");
+		assertNotNull(account);
+		
 
 		
 	}
+	@Test
+	public void testLogIn(){
+	accountCtrl.createAccount("Regine", "regine@gmail.com", "password");
+	accountCtrl.login("regine@gmail.com", "password");
+	assertTrue(accountCtrl.isUserLoggedIn("regine@gmail.com"));
 	
+	
+	
+		
+	}
 }
